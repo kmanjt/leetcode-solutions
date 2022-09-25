63247f039a11307cbb796ead4a3c3c76b18037aa
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2022-09-25 20:53:29 +0100

    Update Valid Parentheses.py

--- Easy/Valid Parentheses.py
+++ Easy/Valid Parentheses.py
@@ -1,33 +1,31 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        
         parentheses = {
-            ")": "(",
-            "]": "[",
-            "}": "{",
+            ")":"(",
+            "]":"[",
+            "}":"{"
         }
         
-        if len(s) == 1:
+        if len(s) < 2:
             return False
-    
-        for char in s:
+        
+        for i, char in enumerate(s):
             if char in parentheses.values():
                 stack.append(char)
-                pass
-             
+            
             elif char in parentheses:
                 if len(stack) < 1:
                     return False
+                
                 elif parentheses[char] == stack[-1]:
                     stack.pop()
+                
                 else:
                     return False
-
-        
+                
+            
         if len(stack) == 0:
             return True
         else:
             return False
-        
-                
